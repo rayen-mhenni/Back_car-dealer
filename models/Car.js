7116dcbe881eeb2e21375a368dcefd198571e444
rayen-mhenni <mhennirayen2@gmail.com>
2023-10-04 11:23:18 +0100

    ff

--- models/Car.js
+++ models/Car.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
 const CarSchema = new mongoose.Schema(
-    {
-        name: { type: String },
-        Make: { type: String },
-        Model: { type: String },
-        Year: { type: String },
-        Mileage: { type: String },
-        Engine: { type: String },
-        Cylinder: { type: String },
-        Transmission: { type: String },
-        Bodytype: { type: String },
-        INTERIORCOLOR: { type: String },
-        EXTERIORCOLOR: { type: String },
-        Price: { type: String },
-        description: { type: String },
-        images: { type: Array},
-    },
+  {
+    name: { type: String },
+    Make: { type: String },
+    Model: { type: String },
+    Year: { type: String },
+    Mileage: { type: String },
+    Engine: { type: String },
+    Cylinder: { type: String },
+    Transmission: { type: String },
+    Bodytype: { type: String },
+    INTERIORCOLOR: { type: String },
+    EXTERIORCOLOR: { type: String },
+    Price: { type: String },
+    description: { type: String },
+    images: { type: Array },
+    options: { type: Array },
+  },
 
-
-    {
-        timestamps: { currentTime: () => Date.now() },
-    }
+  {
+    timestamps: { currentTime: () => Date.now() },
+  }
 );
 
 module.exports = mongoose.model("Car", CarSchema);
